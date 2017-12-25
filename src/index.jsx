756987ce77cd340e530cc85d357a15ca7d260587
Greg Bergé <berge.greg@gmail.com>
2017-12-25 13:24:19 +0100

    Modernize project

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,11 @@
 // @flow
 
-import { AppContainer } from 'react-hot-loader';
-import VectorWidget from './VectorWidget';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader'
+import VectorWidget from './VectorWidget'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const holderDiv = document.createElement('div');
-document.body.appendChild(holderDiv);
+const holderDiv = document.createElement('div')
+document.body.appendChild(holderDiv)
 
-ReactDOM.render(
-  <AppContainer>
-    <VectorWidget />
-  </AppContainer>,
-  holderDiv);
-
-if (module.hot) {
-  module.hot.accept('./VectorWidget', () => {
-    const NextApp = require('./VectorWidget').default;
-    ReactDOM.render(
-      <AppContainer>
-        <NextApp />
-      </AppContainer>,
-      holderDiv);
-  });
-}
+ReactDOM.render(<VectorWidget />, holderDiv)
--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,12 @@ const path = require('path');
 
 module.exports = {
   devtool: 'source-map',
-  entry: [
-    'webpack-dev-server/client?http://0.0.0.0:8080',
-    'webpack/hot/only-dev-server',
-
-    // With the following line uncommented, loading the page gives the following errors in the console:
-    // warning.js:44Warning: Group(...): A valid React element (or null) must be returned. You may have returned undefined, an array or some other invalid object.warning @ warning.js:44
-    // invariant.js:45Uncaught Invariant Violation: Group(...): A valid React element (or null) must be returned. You may have returned undefined, an array or some other invalid object.
-    //
-    // If you comment it out, the page loads and displays the React logo
-    'react-hot-loader/patch',
-
-    './src/index.jsx',
-  ],
+  entry: ['./src/index.jsx',],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       include: path.join(__dirname, 'src'),
-      loader: 'babel-loader',
+      use: 'babel-loader',
     }]
   },
   output: {
@@ -35,7 +23,6 @@ module.exports = {
     }),
   ],
   resolve: {
-    extensions: ['', '.js', '.jsx'],
-    root: path.join(__dirname, 'node_modules'),
+    extensions: ['.js', '.jsx'],
   },
 };
